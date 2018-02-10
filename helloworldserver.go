e356ad19bd7e272e1c5b46ef4c944f3bc165538b
eyoung8 <eyoung8@binghamton.edu>
2018-02-10 18:28:44 -0500

    Simple go server that responds with a message

---
+++ helloworldserver.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func makeHandlerWithMessage(message string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(message))
+	}
+
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Println("Wrong number of args! Use like <port> <message>")
+		os.Exit(1)
+	}
+	port, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Failed to convert port arg to int, exiting...")
+		panic(err)
+	}
+	message := args[1]
+	fmt.Println("Listening on port:", port)
+	fmt.Println("Responding with message:", message)
+
+	http.HandleFunc("/", makeHandlerWithMessage(message))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		panic(err)
+	}
+}
