a92e0482242934e26dbdb76994a1a92d220b8ac9
eyoung8 <eyoung8@binghamton.edu>
2018-02-10 18:40:12 -0500

    Refactor code to functions

--- helloworldserver.go
+++ helloworldserver.go
@@ -7,6 +7,24 @@ import (
 	"strconv"
 )
 
+func validateArgs(args []string) {
+	if len(args) != 2 {
+		fmt.Println("Wrong number of args! Use like <port> <message>")
+		os.Exit(1)
+	}
+}
+
+func getPortAndMessage(args []string) (port int, message string) {
+	validateArgs(args)
+	port, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Failed to convert port arg to int, exiting...")
+		panic(err)
+	}
+	message = args[1]
+	return
+}
+
 func makeHandlerWithMessage(message string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte(message))
@@ -14,23 +32,16 @@ func makeHandlerWithMessage(message string) func(http.ResponseWriter, *http.Requ
 
 }
 
-func main() {
-	args := os.Args[1:]
-	if len(args) != 2 {
-		fmt.Println("Wrong number of args! Use like <port> <message>")
-		os.Exit(1)
-	}
-	port, err := strconv.Atoi(args[0])
-	if err != nil {
-		fmt.Println("Failed to convert port arg to int, exiting...")
-		panic(err)
-	}
-	message := args[1]
+func startServer(port int, message string) {
 	fmt.Println("Listening on port:", port)
 	fmt.Println("Responding with message:", message)
-
 	http.HandleFunc("/", makeHandlerWithMessage(message))
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
+}
+func main() {
+	args := os.Args[1:]
+	port, message := getPortAndMessage(args)
+	startServer(port, message)
 }
